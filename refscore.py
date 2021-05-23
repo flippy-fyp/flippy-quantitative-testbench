15e2cd13284e7db67124b0abc3bae78226c9ea65
Lin Lee <lhlee888@hotmail.com>
2021-05-24 00:43:17 +0100

    Add offset_ticks in score and refscore

--- refscore.py
+++ refscore.py
@@ -8,7 +8,7 @@ TICKS_PER_BEAT = 48
 
 
 def ms_to_tick_gap_and_note_off(
-    notes: List[Tuple[float, int]]
+    notes: List[Tuple[float, int]], offset_ticks: int
 ) -> List[Tuple[int, int]]:
     res: List[Tuple[int, int]] = []
     # first convert to absolute ticks
@@ -20,8 +20,10 @@ def ms_to_tick_gap_and_note_off(
         for (tru_time, midi_note_num) in notes
     ]
 
-    # add the negative notes 32 ticks after--these are the 'note_off' messages
-    off_notes_ticks = [(t + 64, -midi_note_num) for (t, midi_note_num) in notes_ticks]
+    # add the negative notes offset_ticks ticks after--these are the 'note_off' messages
+    off_notes_ticks = [
+        (t + offset_ticks, -midi_note_num) for (t, midi_note_num) in notes_ticks
+    ]
     notes_ticks.extend(off_notes_ticks)
     # sort by tick
     all_notes = sorted(notes_ticks, key=lambda x: x[0])
@@ -36,7 +38,7 @@ def ms_to_tick_gap_and_note_off(
     return res
 
 
-def process_notes(notes: List[Tuple[float, int]]) -> MidiFile:
+def process_notes(notes: List[Tuple[float, int]], offset_ticks: int) -> MidiFile:
     mid = MidiFile(ticks_per_beat=TICKS_PER_BEAT)
     track = MidiTrack()
 
@@ -46,7 +48,7 @@ def process_notes(notes: List[Tuple[float, int]]) -> MidiFile:
     track.append(Message("program_change", program=0))
 
     # first convert to absolute ticks
-    notes_ticks = ms_to_tick_gap_and_note_off(notes)
+    notes_ticks = ms_to_tick_gap_and_note_off(notes, offset_ticks)
     for tick, midi_note_num in notes_ticks:
         msg_type = "note_on" if midi_note_num >= 0 else "note_off"
         track.append(
@@ -66,6 +68,12 @@ if __name__ == "__main__":
         "--input", type=str, help="Input Score file path", required=True
     )
     parser.add_argument("--output", type=str, help="Output file path", required=True)
+    parser.add_argument(
+        "--offset_ticks",
+        type=int,
+        help="Number of MIDI ticks to place note offsets after their respective onsets.",
+        default=64,
+    )
 
     args = parser.parse_args()
     input_path = args.input
@@ -74,5 +82,5 @@ if __name__ == "__main__":
     score = process_ref_file(input_path)
     notes = [(x["tru_time"], x["midi_note_num"]) for x in score]
 
-    mid = process_notes(notes)
+    mid = process_notes(notes, args.offset_ticks)
     mid.save(output_path)
--- score.py
+++ score.py
@@ -9,6 +9,12 @@ if __name__ == "__main__":
         "--input", type=str, help="Input Score file path", required=True
     )
     parser.add_argument("--output", type=str, help="Output file path", required=True)
+    parser.add_argument(
+        "--offset_ticks",
+        type=int,
+        help="Number of MIDI ticks to place note offsets after their respective onsets.",
+        default=64,
+    )
 
     args = parser.parse_args()
     input_path = args.input
@@ -17,5 +23,5 @@ if __name__ == "__main__":
     score = process_score_file(input_path)
     notes = [(x["note_start"], x["midi_note_num"]) for x in score]
 
-    mid = process_notes(notes)
+    mid = process_notes(notes, args.offset_ticks)
     mid.save(output_path)
